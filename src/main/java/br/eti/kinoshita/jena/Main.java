45a293eaf1a1089c55ddd03c507942bed9e68cf3
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2017-04-29 23:15:16 +1200

    Add example with multiple language tags

--- src/main/java/br/eti/kinoshita/jena/Main.java
+++ src/main/java/br/eti/kinoshita/jena/Main.java
@@ -13,7 +13,7 @@ import org.apache.jena.rdf.model.ModelFactory;
 
 public class Main {
 
-    public static void main(String[] args) throws Throwable {
+    public static void main1(String[] args) throws Throwable {
         // Query String
         final String queryString = "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n" + 
                 "SELECT ?label\n" + 
@@ -26,6 +26,31 @@ public class Main {
         // --- Model
         Model model = ModelFactory.createDefaultModel();
         try (InputStream is = Main.class.getResourceAsStream("/ysa-skos.ttl")) {
+            model.read(is, /* base */null, "TURTLE");
+        }
+        // Query object
+        Query query = QueryFactory.create(queryString);
+        // Execute query
+        try (QueryExecution qExec = QueryExecutionFactory.create(query, model)) {
+            ResultSet results = qExec.execSelect();
+            while (results.hasNext()) {
+                QuerySolution solution = results.nextSolution();
+                System.out.println(solution);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Throwable {
+        // Query String
+        final String queryString = "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n" + 
+                "SELECT ?label\n" + 
+                "WHERE {\n" + 
+                "  ?any skos:prefLabel|skos:altLabel ?label .\n" + 
+                "} \n" + 
+                "ORDER BY ?label";
+        // --- Model
+        Model model = ModelFactory.createDefaultModel();
+        try (InputStream is = Main.class.getResourceAsStream("/vocabularies.ttl")) {
             model.read(is, /* base */null, "TURTLE");
         }
         // Query object
